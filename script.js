7250d3bc0e2bf3e881601dd4f0609691e812a600
poojarna <poojarna@mail.uc.edu>
2025-11-05 20:59:51 -0500

    Added files for the code.

---
+++ script.js
@@ -0,0 +1,23 @@
+// I think I need to make a code for inputs? 
+submitNow.on("click",getNotes);
+
+function getNotes(){
+  event.preventDefault();
+  var food = ${'food'}.val();
+  var email = ${'email'}.val();
+  var color = ${'color'}.val();
+  var animal = ${'animal'}.val();
+  console.log(food);
+  console.log(email);
+  console.log(color);
+  console.log(animal);
+}
+//I can't believe this got tougher. I think on Fridays, I can take advantage.
+
+// I loved Sandra Cheah's work, so I took influence
+
+// The only step that was missing was adding an if/else statement for my code 
+
+// PS The code finally came out fine!!
+
+// Where is the syntax error? 
